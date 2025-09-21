3948164e75f5c3451a1b60471d3dd772d64dc087
Jonas Rathert <jrt@gmx.de>
2025-09-22 00:27:19 +0200

    fix: prepare texts for 2026 edition

---
+++ src/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+// see https://symfony.com/doc/current/best_practices.html#use-constants-to-define-options-that-rarely-change
+class Config
+{
+    public const SWK_YEAR = '2026';
+}
